7fdcde40cf416e07429abea8ce632a2817d114a2
Tiago "Dosaki" Correia <dosaki@users.noreply.github.com>
2019-08-10 14:25:30 +0100

    Ignoring exp4j

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ var drawProject = function (project, sha, specialProjectPages) {
 
 
 var projectPages = ["picons"];
-var ignoreProjects = ["panintelligence.github.io"];
+var ignoreProjects = ["panintelligence.github.io", "exp4j];
 var projectList = document.getElementById("project-list");
 github.projects(function (projectsInfo) {
     projectList.innerHTML = "";
