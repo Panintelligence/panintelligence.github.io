2fcb330057fcb7973a5ba1280eb20d212d862022
Tiago Correia <tiago.f.a.correia@gmail.com>
2021-02-15 14:40:14 +0000

    Restricting projects. Updated branding

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 var drawProject = function (project, sha, specialProjectPages) {
     var name = project['name'];
-    if (ignoreProjects)
-        var description = project['description'];
+    var description = project['description'];
     var link = project['html_url'];
     var imageUrl = "https://rawcdn.githack.com/Panintelligence/" + name + "/" + sha + "/changes/images/" + name + "-logo.svg"
     var link = specialProjectPages.indexOf(name) > -1 ? (name + ".html") : ("https://github.com/Panintelligence/" + name)
@@ -33,13 +32,13 @@ var drawProject = function (project, sha, specialProjectPages) {
 
 
 var projectPages = ["picons"];
-var ignoreProjects = ["panintelligence.github.io", "exp4j", "installation-example-node"];
+var showcaseProjects = ["henchman", "picons", "document.cookie.mock"];
 var projectList = document.getElementById("project-list");
 github.projects(function (projectsInfo) {
     projectList.innerHTML = "";
     for (var i = 0; i < projectsInfo.length; i++) {
         var projectName = projectsInfo[i]['name'];
-        if (projectsInfo[i] && ignoreProjects.indexOf(projectName) === -1) {
+        if (projectsInfo[i] && showcaseProjects.indexOf(projectName) !== -1) {
             (function(proj){
                 github.ref(projectName, function (refInfo) {
                     var elem = drawProject(proj, refInfo.object['sha'], projectPages);
